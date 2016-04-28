565503e9d12d470c08568d846142e3692af16c97
louislzcute <ll.khac.quan@gmail.com>
2016-04-28 14:23:04 +0700

    Use "parallel for" for download multiple chapter at once

--- Stories Grab/MainForm.cs
+++ Stories Grab/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Stories_Grab
@@ -11,6 +12,7 @@ namespace Stories_Grab
     {
         BackgroundWorker bw;
         LogHandler logger;
+        List<string> chapterUrls;
 
         public MainForm()
         {
@@ -59,7 +61,7 @@ namespace Stories_Grab
         private void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             toolStripProgressBar.Value = e.ProgressPercentage;
-            toolStripStatusLabel.Text = e.UserState.ToString();
+            toolStripStatusLabel.Text = string.Format("{0}/{1}", e.UserState, chapterUrls.Count);
         }
 
         private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
@@ -71,27 +73,51 @@ namespace Stories_Grab
 
         private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-            IGrapper grapper = new ISachInfo();
+            IGrapper grapper;
             string url = storyLinkText.Text;
+            if (url.Contains("isach.info"))
+                grapper = new ISachInfo();
+            else if (url.Contains("truyenfull.vn"))
+                grapper = new TruyenFullGrapper();
+            else
+            {
+                MessageBox.Show(url + " is not suppoted");
+                return;
+            }
             int pages = (int)nPagesOfMenu.Value;
-            List<string> chapterUrls = grapper.GetChapterLinks(url, 1, pages);
-            for (int i = 0; i < chapterUrls.Count; i++)
+            chapterUrls = grapper.GetChapterLinks(url, 1, pages);
+
+            int iCompleted = 0;
+            Parallel.For(0, chapterUrls.Count, (i, loopState) =>
             {
                 string s = grapper.GetCorespondingFileName(chapterUrls[i]);
                 string filePath = string.Format("{0}\\{1}", folderTextBox.Text, s);
                 bool exists = File.Exists(filePath);
                 if (exists)
                 {
-                    logger.log(s + " exists. Skip:");
-                    continue;
+                    lock (logger)
+                    {
+                        logger.log(s + " exists. Skip:");
+                    }
                 }
-                Chapter c = grapper.GrabChapterContent(chapterUrls[i]);
-                c.writeToFile(filePath);
-                logger.log(s + " is written");
-                bw.ReportProgress(i * 100 / chapterUrls.Count, i);
-                if (bw.CancellationPending)
-                    return;
-            }
+                else
+                {
+                    Chapter c = grapper.GrabChapterContent(chapterUrls[i]);
+                    c.writeToFile(filePath);
+                    lock (logger)
+                    {
+                        logger.log(s + " is written");
+                    }
+                    
+                    if (bw.CancellationPending)
+                    {
+                        loopState.Stop();
+                        return;
+                    }
+                }
+                iCompleted++;
+                bw.ReportProgress(iCompleted * 100 / chapterUrls.Count, iCompleted);
+            });
         }
 
         private void folderTextBox_DoubleClick(object sender, EventArgs e)
